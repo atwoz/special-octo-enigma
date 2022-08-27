9c8a1f547f58908b0f97837116ef0dc6e5aecfab
wozix <arturo@arteko.mx>
2022-08-27 10:26:59 -0500

    Adds blank env.

---
+++ env.blank.js
@@ -0,0 +1,11 @@
+const token = '';
+const url = 'http://localhost:8086'
+const bucket = ''
+const org = ''
+const endpoint = 'http://192.168.4.1/getVal';
+
+exports.token = token;
+exports.url = url;
+exports.bucket = bucket
+exports.org = org;
+exports.endpoint = endpoint;
